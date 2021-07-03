49708e0f39b65ac497a22a55e71ced1275bd382b
Bernardo Paulsen <bernardohpaulsen@gmail.com>
2021-07-03 13:20:57 -0300

    Now when the program runs out of artworks to post it runs the scraper and updates the data pickle

--- __main__.py
+++ __main__.py
@@ -1,13 +1,16 @@
 import bot
+import get_data
 import time
 
 def main():
     four_hours = 4*60*60 #seconds
     while True:
         text, url , more = bot.get_random_post() #get data
-        if not more: break
-        bot.tweet_image(text, url)
-        time.sleep(four_hours)
+        if more:
+            bot.tweet_image(text, url)
+            time.sleep(four_hours)
+        else:
+            get_data.run()
 
 if __name__ == "__main__":
     main()
--- get_data.py
+++ get_data.py
@@ -3,29 +3,30 @@ import requests as req
 import logging
 import pickle
 
-logging.basicConfig(level=0)
+def run():
+    logging.basicConfig(level=0)
 
-data = []
-             
-html = req.get('https://masp.org.br/acervo/busca?author=')     # this link returns the entire archive
-soup = BeautifulSoup(html.text, 'lxml')
-soup = soup.find('section', class_='container margin-top row') # gets only the part of the page where the art is
+    data = []
+                
+    html = req.get('https://masp.org.br/acervo/busca?author=')     # this link returns the entire archive
+    soup = BeautifulSoup(html.text, 'lxml')
+    soup = soup.find('section', class_='container margin-top row') # gets only the part of the page where the art is
 
-artists = soup.find_all('h2', class_='title') # h2 = where the name of the artist is
+    artists = soup.find_all('h2', class_='title') # h2 = where the name of the artist is
 
-for artist in artists:    
-    work        = artist.find_next('div', class_='col-xs-12 col-md-12 no-padding') # gets the block where the images are
-    artist_name = artist.text.rstrip()
-    figures     = work.find_all('figure')
-    for figure in figures:
-        data.append({
-        'artist'       : artist_name,
-        'website url'  : figure.a.get('href'),
-        'image url'    : figure.img.get('src'),
-        'title & year' : figure.p.text
-        })
-    logging.info(f"completed: {artist_name}")
+    for artist in artists:    
+        work        = artist.find_next('div', class_='col-xs-12 col-md-12 no-padding') # gets the block where the images are
+        artist_name = artist.text.rstrip()
+        figures     = work.find_all('figure')
+        for figure in figures:
+            data.append({
+            'artist'       : artist_name,
+            'website url'  : figure.a.get('href'),
+            'image url'    : figure.img.get('src'),
+            'title & year' : figure.p.text
+            })
+        logging.info(f"completed: {artist_name}")
 
-with open("data.pickle","wb") as file:
-    pickle.dump(data, file)
-    logging.debug("Data saved")
+    with open("data.pickle","wb") as file:
+        pickle.dump(data, file)
+        logging.debug("Data saved")
